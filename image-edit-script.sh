7189135fada0840f21089b2bb3e668ee5c67dbda
abj-paul <bsse1201@iit.du.ac.bd>
2022-01-16 19:09:15 +0600

    Trying to categorize based on image size

--- image-edit-script.sh
+++ image-edit-script.sh
@@ -4,7 +4,23 @@
 cat names.txt | while read NAME 
 do
    # do something with $line here
-convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 930,800 '$NAME'" image1.png "$NAME".png
+size=${#NAME} 
+if ((size <=8))
+then
+    convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 950,800 '$NAME'" image1.png "$NAME".png
+elif ((size <=15))
+then
+    convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 850,800 '$NAME'" image1.png "$NAME".png
+elif ((size <=22))
+then
+    convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 800,800 '$NAME'" image1.png "$NAME".png
+elif ((size <=29))
+then
+    convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 750,800 '$NAME'" image1.png "$NAME".png
+elif ((size <=40))
+then
+    convert -font  Flaemische-Kanzleischrift -fill black -pointsize 170 -draw "text 700,800 '$NAME'" image1.png "$NAME".png
+fi
 echo "Completed certificate for $NAME"
 
 done
